3e114acd299c8d21633224cac841aaca5f195ec8
Lynsey Kemplin <purplez479@gmail.com>
2022-09-10 14:01:56 -0700

    added display assignment

--- Fronts.js
+++ Fronts.js
@@ -35,19 +35,38 @@ class LinkedList {
         }
         console.log(this.head.data)
     }
+
+    display() {
+        if (!this.head) {
+            console.log("null")
+            return null
+        }
+        let runner = this.head
+        while (runner !== null) {
+            console.log(this.head.data)
+            runner = runner.next;
+        }
+    }
 }
 
-SLL1 = new LinkedList();
-SLL1.addFront(18)
-console.log(SLL1)
-SLL1.addFront(5)
-console.log(SLL1)
-SLL1.addFront(73)
-console.log(SLL1)
-SLL1.removeFront();
-console.log(SLL1)
-SLL1.removeFront();
-console.log(SLL1)
-SLL1.front();
-SLL1.removeFront();
-SLL1.front();
+// SLL1 = new LinkedList();
+// SLL1.addFront(18)
+// console.log(SLL1)
+// SLL1.addFront(5)
+// console.log(SLL1)
+// SLL1.addFront(73)
+// console.log(SLL1)
+// SLL1.removeFront();
+// console.log(SLL1)
+// SLL1.removeFront();
+// console.log(SLL1)
+// SLL1.front();
+// SLL1.removeFront();
+// SLL1.front();
+
+SLL1 = new LinkedList()
+SLL1.addFront(76)
+SLL1.addFront(2)
+SLL1.display()
+SLL1.addFront(11.41)
+SLL1.display()
